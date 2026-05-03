05d4caa7044f8861db40a61777a32ce5f8788f4e
Ali Sol <alisolphp@gmail.com>
2026-05-03 16:43:22 +0330

    Clean source code for F-Droid submission

---
+++ app/src/main/java/ir/alisolphp/echotalk/DelegationService.java
@@ -0,0 +1,14 @@
+package ir.alisolphp.echotalk;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
